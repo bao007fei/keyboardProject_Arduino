89da6ace0ec79ad42f6da89bc1e67c5268704df1
bao007fei <bao007fei@gmail.com>
2020-08-01 03:18:10 +0800

    Revert "format"
    This reverts commit 955f73c4e17fd9d33864fa61b177bfcbd9f84c22.

--- keyboard.ino
+++ keyboard.ino
@@ -1,37 +1,38 @@
 int read = 1;
 char chars[6][5] = {
-    {'a', 'b', 'c', 'd', 'e'},
-    {'f', 'g', 'h', 'i', 'j'},
-    {'k', 'l', 'm', 'n', 'o'},
-    {'p', 'q', 'r', 's', 't'},
-    {'u', 'v', 'w', 'x', 'y'},
-    {'z', 'z', 'z', 'z', 'z'}};
+                {'a', 'b', 'c', 'd', 'e'},
+                {'f', 'g', 'h', 'i', 'j'},
+                {'k', 'l', 'm', 'n', 'o'},
+                {'p', 'q', 'r', 's', 't'},
+                {'u', 'v', 'w', 'x', 'y'},
+                {'z', 'z', 'z', 'z', 'z'}
+             };
 
-void setup()
-{
+
+void setup() {
   // put your setup code here, to run once:
-  for (int i = 0; i <= 10; i++)
-  {
+  for(int i = 0; i <= 10; i++){
     pinMode(i, OUTPUT);
   }
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i <= 5; i++)
-  {
-    for (int j = 6; j <= 10; j++)
-    {
+  for(int i = 0; i <= 5; i++){
+    for(int j = 6; j <= 10; j++){
       digitalWrite(i, LOW);
       digitalWrite(j, HIGH);
 
       read = digitalRead(j);
-      if (read == LOW)
-      {
+      if(read == LOW){
         Serial.println(chars[i][j]);
         delay(1000);
       }
+      
     }
   }
+
+  
+
+  
 }
